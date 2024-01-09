557e0d3d957481338613ebda0c8171295cb480da
Gonzalo Araya <gonxaraya991@gmail.com>
2024-01-09 19:16:45 -0300

    configuracion de la app en el main

--- src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -1,13 +1,27 @@
 package utn.tienda_libros;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import utn.tienda_libros.vista.LibroForm;
+
+import java.awt.*;
 
 @SpringBootApplication
 public class TiendaLibrosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TiendaLibrosApplication.class, args);
-	}
+		ConfigurableApplicationContext contextoSpring = (ConfigurableApplicationContext)
+				new SpringApplicationBuilder(TiendaLibrosApplication.class)
+						.headless(false).web(WebApplicationType.NONE).run(args);
 
+		// Ejecutamos el codigo para cargar el formulario
+		EventQueue.invokeLater(() -> {
+			// obtenemos elñ objeto form a través de spring
+			LibroForm libroForm = contextoSpring.getBean(LibroForm.class);
+			libroForm.setVisible(true);
+		});
+	}
 }
